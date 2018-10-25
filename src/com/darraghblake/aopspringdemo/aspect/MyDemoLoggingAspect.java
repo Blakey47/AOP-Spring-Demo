62dd7c59cf44b154d3d9f0dfac2a7a1309cdcde9
Darragh Blake <darragh.blake48@gmail.com>
2018-10-25 14:18:27 +0100

    Adding AOP Aspect Support

---
+++ src/com/darraghblake/aopspringdemo/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,18 @@
+package com.darraghblake.aopspringdemo.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class MyDemoLoggingAspect {
+	
+	// This is where we add all of our related advices for logging
+	
+	@Before("execution(public void addAccount())")
+	public void beforeAddAccountAdvice() {
+		System.out.println("LOGGING DETAILS: Executing @Before advice on addAccount()");
+	}
+	
+}
